b10373b634012c84440dec0aa7ba489c1f1d82fc
GiorgiJamaspishvili <126399819+GiorgiJamaspishvili@users.noreply.github.com>
2023-05-25 16:09:32 +0400

    up_up

--- script.js
+++ script.js
@@ -42,47 +42,34 @@ fetch('https://reqres.in/api/users?page=2', {
 
 
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'GET'
-})
-.then(function (getDataInfo) {
-   
-    return getDataInfo.json();
-})
+let mainDiv = document.querySelector(".post_block");
 
-.then(function(receivedInfo){
-    let ulItem2 = document.createElement('ul');
-    receivedInfo.forEach(element2 => {
-        // createPost(element2);
-        let liItem2 = document.createElement('li');
-            let dataInfo_2 = document.createElement('p');
-            dataInfo_2.innerText = `${element2.title}`
-            ulItem2.appendChild(liItem2);
-            ulItem2.appendChild(dataInfo_2);
-       
-        
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "GET",
+})
+  .then(function (mosuliinfo) {
+    return mosuliinfo.json();
+  })
+  .then(function (response) {
+    response.forEach((element) => {
+        createPost(element);
     });
-    document.getElementById('post_block').appendChild(ulItem2);
-})
-
-.catch(function (r2rsp) {
-    console.log(r2rsp);
-})
-
-
+    console.log(response);
+  })
+  .catch(function (errorResponse) {
+    console.log(errorResponse);
+  });
 
 function createPost(item) {
-    let divWraper = document.createElement('div');
-  
-    let h2Tag = document.createElement('h2');
-    h2Tag.innerText = `${item.id}`;
-  
-    let h3Tag = document.createElement('h3');
-    h3Tag.innerText = `${item.title}`;
-  
-    divWraper.appendChild(h2Tag);
-    divWraper.appendChild(h3Tag);
-  
-    mainPostWraper.appendChild(divWraper);
-    console.log(divWraper);
-  }
+  let divWraper = document.createElement("div");
+  let h2Teg = document.createElement("h2");
+  h2Teg.innerText = `${item.id}`;
+  let h3Teg = document.createElement("h3");
+  h3Teg.innerText = `${item.title}`;
+
+  divWraper.appendChild(h2Teg);
+  divWraper.appendChild(h3Teg);
+
+  mainDiv.appendChild(divWraper);
+  console.log(divWraper);
+}
