d592a9440cb47e5cae69a8c66a1b16fb65dec07c
GiorgiJamaspishvili <126399819+GiorgiJamaspishvili@users.noreply.github.com>
2023-05-25 15:55:14 +0400

    up

--- script.js
+++ script.js
@@ -39,3 +39,50 @@ fetch('https://reqres.in/api/users?page=2', {
     })
 
 
+
+
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'GET'
+})
+.then(function (getDataInfo) {
+   
+    return getDataInfo.json();
+})
+
+.then(function(receivedInfo){
+    let ulItem2 = document.createElement('ul');
+    receivedInfo.forEach(element2 => {
+        // createPost(element2);
+        let liItem2 = document.createElement('li');
+            let dataInfo_2 = document.createElement('p');
+            dataInfo_2.innerText = `${element2.title}`
+            ulItem2.appendChild(liItem2);
+            ulItem2.appendChild(dataInfo_2);
+       
+        
+    });
+    document.getElementById('post_block').appendChild(ulItem2);
+})
+
+.catch(function (r2rsp) {
+    console.log(r2rsp);
+})
+
+
+
+function createPost(item) {
+    let divWraper = document.createElement('div');
+  
+    let h2Tag = document.createElement('h2');
+    h2Tag.innerText = `${item.id}`;
+  
+    let h3Tag = document.createElement('h3');
+    h3Tag.innerText = `${item.title}`;
+  
+    divWraper.appendChild(h2Tag);
+    divWraper.appendChild(h3Tag);
+  
+    mainPostWraper.appendChild(divWraper);
+    console.log(divWraper);
+  }
